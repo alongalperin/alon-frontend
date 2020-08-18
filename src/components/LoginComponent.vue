30c1ea4a4555e147348ddc2e53342e00102f825b
Alon Galperin <alonga90@gmail.com>
2020-08-18 13:21:23 +0200

    Basic login with post request

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
-    <h1>I am Login</h1>
+    <h1>Login</h1>
+    <form @submit.prevent="onSubmit">
+      <label for="email">Email:</label><br />
+      <input type="text" v-model="email" id="email" name="email" /><br />
+      <label for="password">Password:</label><br />
+      <input type="text" v-model="password" id="password" name="password" />
+      <input type="submit" value="Submit" />
+    </form>
+    <p>{{ this.error }}</p>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: "",
+    };
+  },
   components: {},
+  methods: {
+    onSubmit: async function() {
+      try {
+        const response = await axios.post(
+          "https://sandbox.d.greeninvoice.co.il/api/v1/account/login",
+          {
+            email: this.email,
+            password: this.password,
+          }
+        );
+        console.log(response.headers["x-authorization-bearer"]);
+      } catch (err) {
+        this.error = "somwthing went wrong";
+      }
+    },
+  },
 };
 </script>
 
