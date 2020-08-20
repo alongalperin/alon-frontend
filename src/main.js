3115e99ed2341a29829c0a7a9ffa90e46645a91d
Alon Galperin <alonga90@gmail.com>
2020-08-20 19:00:10 +0200

    Return hashtag to router

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({ routes, mode: "history" });
+const router = new VueRouter({ routes });
 
 store.dispatch("INIT_APP_USER_TOKEN");
 
